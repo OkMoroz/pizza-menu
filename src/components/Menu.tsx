8ca50c6e5cd8e7bb4d880deb8ea84b354a0bee73
OkMoroz <k.moroz.mail@gmail.com>
2025-05-15 18:58:14 +0300

    fix columns

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,7 +5,7 @@ const Menu = () => {
     return (
         <main className="menu">
             <h2>Our pizza</h2>
-            <ul>
+            <ul className="pizzas">
                 {pizzaData.map(pizza => (
                     <Pizza pizzaObj={pizza} key={pizza.name} />
                 ))}
