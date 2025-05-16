ad9a890416559eb8fe0cb6bd64c43fc3895b577c
OkMoroz <k.moroz.mail@gmail.com>
2025-05-16 11:27:43 +0300

    change menu styles in TW

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,9 +3,11 @@ import Pizza from './Pizza';
 
 const Menu = () => {
     return (
-        <main className="menu">
-            <h2>Our pizza</h2>
-            <ul className="pizzas">
+        <main className="flex flex-col items-center gap-16">
+            <h2 className="inline-block border-t-2 border-b-2 border-current uppercase tracking-[3px] font-medium text-5xl">
+                Our pizza
+            </h2>
+            <ul className="grid grid-cols-1 md:grid-cols-2 gap-12 list-none">
                 {pizzaData.map(pizza => (
                     <Pizza pizzaObj={pizza} key={pizza.name} />
                 ))}
@@ -15,3 +17,4 @@ const Menu = () => {
 };
 
 export default Menu;
+
